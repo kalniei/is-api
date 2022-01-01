051ea8345a8ace5e3ebb3703ef7bb2415b2a5a83
Olia <olia@lifeinmobile.com>
2022-01-01 19:34:41 +0100

    remove log

--- api/getEmailTemplates.js
+++ api/getEmailTemplates.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log(req);
   const getTableModule = require("./helpers/getTable");
   getTableModule.getTable(res, "admin_email_templates");
 });
