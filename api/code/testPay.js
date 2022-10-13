351c5d43e9ce0ee1bfb950810f21fbe0ba0812b6
Olia Kalniei <olia@lifeinmobile.com>
2022-10-13 12:18:06 +0200

    test pay - send emial

--- api/code/testPay.js
+++ api/code/testPay.js
@@ -5,11 +5,27 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   console.log(1111111);
   console.log(req.body);
-  res.json({
-    status: 200,
-    message: "Success",
-    data: 'jest git',
-  });  
+  const sendSingleEmail = require("../email-sender/sendSingleEmail");
+
+  const returnError = require("../common-helpers/returnError");
+
+  try {
+    await sendSingleEmail({
+      from: '"Impro Silesia" biuro@improsilesia.pl',
+      to: 'olga.kalniei@gmail.com',
+      subject: 'ho ho ho',
+      html: 'this is content',
+    });
+    res.json({
+      status: 200,
+      message: "Email has been sent",
+      data: null,
+    });
+  } catch (error) {
+    return returnError(res, error);
+  }
+
+  
 });
 
 module.exports = router;
