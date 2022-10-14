b8fd94a9a8c2f9547c1a7ad4b9b0338bebddb859
Olia Kalniei <olia@lifeinmobile.com>
2022-10-14 17:11:26 +0200

    Revert changes - change auth method

--- api/payment/transactionVerification.js
+++ api/payment/transactionVerification.js
@@ -5,90 +5,63 @@ const crypto = require('crypto');
 require("dotenv").config();
 
 router.post("/", async (req, res) => {
-
   const returnError = require("../common-helpers/returnError");
 
-  const sendSingleEmail = require("../email-sender/sendSingleEmail");
+  const basicAuth = 'Basic ' + Buffer.from(req.body.merchantId + ':' + '546850019766733902dd563fb07fafde').toString('base64');
 
+      
+  const tempData = {
+    merchantId: req.body.merchantId,
+    posId: req.body.posId,
+    sessionId: req.body.sessionId,
+    amount: req.body.amount,
+    currency: req.body.currency,
+    orderId: req.body.orderId,
+    sign: crypto.createHash('sha384').update(JSON.stringify({
+      sessionId: req.body.sessionId,
+      orderId: req.body.orderId,
+      amount: req.body.amount,
+      currency: req.body.currency,
+      crc: process.env.CRC_KEY,
+    })).digest("hex")
+  };
+    
+  axios.put(process.env.PAYMENT_API + '/v1/transaction/verify', tempData, {
+    headers: { Authorization: basicAuth }
+  })
+    .then((response) => {
+      res.json({
+        status: 200,
+        message: "Verification of the payment succeed!",
+        data: null,
+      });
+    }).catch((error) => {
+      return returnError(res, error);
+    });
+
+  
+
+  // const sendSingleEmail = require("../email-sender/sendSingleEmail");
+
+  // const returnError = require("../common-helpers/returnError");
 
   // try {
   //   await sendSingleEmail({
   //     from: '"Impro Silesia" biuro@improsilesia.pl',
   //     to: 'olga.kalniei@gmail.com',
-  //     subject: 'THIS email with body',
-  //     html: `this is temp data: ${JSON.stringify(req.body)}`,
+  //     subject: 'THIS IS NEW EMAIL',
+  //     html: `this is request body: ${JSON.stringify(req.body)}`,
   //   });
-   
+  //   res.json({
+  //     status: 200,
+  //     message: "Email has been sent",
+  //     data: null,
+  //   });
   // } catch (error) {
+  //   return returnError(res, error);
   // }
 
-  const basicAuth = 'Basic ' + Buffer.from(req.body.merchantId + ':' + '546850019766733902dd563fb07fafde').toString('base64');
-
-    try {
-      await sendSingleEmail({
-        from: '"Impro Silesia" biuro@improsilesia.pl',
-        to: 'olga.kalniei@gmail.com',
-        subject: 'THIS email with basicAuth',
-        html: `this is temp data: ${basicAuth}`,
-      });
-     
-    } catch (error) {
-    }
-      
-    // const tempData = {
-    //   merchantId: req.body.merchantId,
-    //   posId: req.body.posId,
-    //   sessionId: req.body.sessionId,
-    //   amount: req.body.amount,
-    //   currency: req.body.currency,
-    //   orderId: req.body.orderId,
-    //   sign: crypto.createHash('sha384').update(JSON.stringify({
-    //     sessionId: req.body.sessionId,
-    //     orderId: req.body.orderId,
-    //     amount: req.body.amount,
-    //     currency: req.body.currency,
-    //     crc: '0bdf9c814c299616',
-    //   })).digest("hex")
-    // };
-
-    // try {
-    //   await sendSingleEmail({
-    //     from: '"Impro Silesia" biuro@improsilesia.pl',
-    //     to: 'olga.kalniei@gmail.com',
-    //     subject: 'THIS email with data',
-    //     html: `this is temp data: ${JSON.stringify(tempData)} and this is auth token: ${basicAuth} `,
-    //   });
-     
-    // } catch (error) {
-    // }
-    
-    // axios.put('https://sandbox.przelewy24.pl/api' + '/v1/transaction/verify', tempData, {
-    //   headers: { Authorization: basicAuth }
-    // })
-    // .then((response) => {
-    //   sendSingleEmail({
-    //       from: '"Impro Silesia" biuro@improsilesia.pl',
-    //       to: 'olga.kalniei@gmail.com',
-    //       subject: 'this is email on success',
-    //       html: `no i?`,
-    //     });
-
-    //   res.json({
-    //     status: 200,
-    //     message: "Verification of the payment succeed!",
-    //     data: null,
-    //   });
-    // }).catch((error) => {
-
-    //   sendSingleEmail({
-    //     from: '"Impro Silesia" biuro@improsilesia.pl',
-    //     to: 'olga.kalniei@gmail.com',
-    //     subject: 'this is email on error',
-    //     html: `${JSON.stringify(error)}`,
-    //   });
-
-    //   return returnError(res, error);
-    // });
+  
 });
 
 module.exports = router;
